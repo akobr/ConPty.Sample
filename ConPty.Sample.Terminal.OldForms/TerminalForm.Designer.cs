8ec6bf14cd16ecb4897316ce6e2dacee441173c6
Ales Kobr <kobr.ales@hotmail.cz>
2020-01-27 18:51:45 +0000

    added windows form terminal client (.net framework)

---
+++ ConPty.Sample.Terminal.OldForms/TerminalForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ConPty.Sample.Terminal.OldForms
+{
+    partial class TerminalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOutput.Location = new System.Drawing.Point(0, 0);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOutput.Size = new System.Drawing.Size(800, 450);
+            this.tbOutput.TabIndex = 0;
+            // 
+            // TerminalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbOutput);
+            this.Name = "TerminalForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOutput;
+    }
+}
+
---
+++ ConPty.Sample.Terminal.OldForms/TerminalForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConPty.Sample.Terminal.OldForms
+{
+    public partial class TerminalForm : Form
+    {
+        //private NativeConsole console;
+        private ConsoleApi.Terminal terminal;
+        private bool terminalIsExiting;
+
+        public TerminalForm()
+        {
+            InitializeComponent();
+            InitialiseTerminal();
+        }
+
+        private void InitialiseTerminal()
+        {
+            //console = new NativeConsole(false);
+            terminal = new ConsoleApi.Terminal();
+            terminal.Start(@"ping localhost", 126, 32);
+
+            _ = Task.Run(CopyConsoleToWindow);
+        }
+
+        private void CopyConsoleToWindow()
+        {
+            var buffer = new char[1024];
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(terminal.Output))
+                using (StreamWriter fileWriter = new StreamWriter(File.Open("output.txt", FileMode.Create, FileAccess.Write)) { AutoFlush = true })
+                {
+                    while (true)
+                    {
+                        int readed = reader.Read(buffer, 0, buffer.Length);
+
+                        if (readed == 0)
+                        {
+                            return;
+                        }
+
+                        fileWriter.Write(buffer, 0, readed);
+                        OutputCharacters(buffer, readed);
+                    }
+                }
+            }
+            catch (ObjectDisposedException) { /* Pseudo terminal is terminated. */ }
+            catch (Exception exception)
+            {
+                string message = Environment.NewLine + "Error: " + exception.Message;
+                OutputCharacters(message.ToCharArray(), message.Length);
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            terminalIsExiting = true;
+            terminal?.Dispose();
+            //console?.Dispose();
+        }
+
+        private void OutputCharacters(char[] buffer, int length)
+        {
+            if (terminalIsExiting)
+            {
+                return;
+            }
+
+            string text = new string(buffer.Take(length).ToArray());
+
+            if (CheckForIllegalCrossThreadCalls)
+            {
+                Invoke(new Action(() => { tbOutput.Text += text; }));
+            }
+            else
+            {
+                tbOutput.Text += text;
+            }
+        }
+    }
+}
