c04a23134278ac01d34569c11c92d9360f5e325e
Ales Kobr <kobr.ales@hotmail.cz>
2020-01-27 18:51:27 +0000

    added windows form terminal client (.net core)

---
+++ ConPty.Sample.Terminal.Forms/TerminalForm.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace ConPty.Sample.Terminal.Forms
+{
+    partial class TerminalForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox tbOutput;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Terminal";
+            // 
+            // tbOutput
+            //
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOutput.Location = new System.Drawing.Point(10, 10);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOutput.Size = new System.Drawing.Size(500, 300);
+            this.tbOutput.TabIndex = 0;
+            this.Controls.Add(this.tbOutput);
+        }
+
+        #endregion
+    }
+}
+
